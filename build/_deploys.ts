77a4b95223bde6530593dfe33cda4ca4a2da1b6d
HAY <hayitaliotamurotov@gmail.com>
2024-11-29 16:18:39 +0500

    Update and rename _deploy.ts to _deploys.ts

---
+++ build/_deploys.ts
@@ -0,0 +1,27 @@
+> @ton-defi.org/jetton-deployer-contracts@0.0.2 deploy
+> ts-node ./build/_deploy.ts
+
+=================================================================
+Deploy script running, let's find some contracts to deploy..
+
+* We are working with 'mainnet'
+
+* Config file '.env' found and will be used for deployment!
+ - Wallet address used to deploy from is: EQAdWzm1d_Jxqq-9_CEXIQ04zwI2-IYh3fFtG1PYi9V0rW5w
+ - Wallet balance is YOUR-BALANCE TON, which will be used for gas
+
+* Found root contract 'build/jetton-minter.deploy.ts - let's deploy it':
+ - Based on your init code+data, your new contract address is: EQAdWzm1d_Jxqq-9_CEXIQ04zwI2-IYh3fFtG1PYi9V0rW5w
+ - Let's deploy the contract on-chain..
+ - Deploy transaction sent successfully
+ - Block explorer link: https://tonwhales.com/explorer/address/EQAdWzm1d_Jxqq-9_CEXIQ04zwI2-IYh3fFtG1PYi9V0rW5w
+ - Waiting up to 20 seconds to check if the contract was actually deployed..
+ - SUCCESS! Contract deployed successfully to address: YOUR-ADDRESS
+ - New contract balance is now YOUR-BALANCE TON, make sure it has enough to pay rent
+ - Running a post deployment test:
+{
+  name: 'HaN',
+  description: 'HAN',
+  image: 'https://avatars.githubusercontent.com/u/175396274?s=400&u=1620902688909376e10478a785ad2e453b3b60ff&v=4',
+  symbol: 'HAN'
+}
